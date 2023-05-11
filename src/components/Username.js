cdc537e166f32b8f8959ba8ac535a4a58b9c5e47
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-05-11 12:47:39 +0530

    Update Username.js

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,1 +1,16 @@
+import { useState } from "react"
+import React from 'react'
 
+function Username() {
+
+    const [name, setName]=useState("")
+  return (
+    <div>
+        <label htmlFor="Username">Enter Your Name: </label><br />
+      <input id="Username" type="text" value={name} onChange={e=>setName(e.target.value)}/>
+      {name ? <h2>Hello {name}!</h2> :<>{name}</>}
+    </div>
+  )
+}
+
+export default Username
